ef7c2fe5dfa8e5ef6cf211c5e61794430e565dab
juanda151005 <98972694+juanda151005@users.noreply.github.com>
2023-04-23 21:46:14 -0500

    Add files via upload

--- ProyectoEafit/inicio.php
+++ ProyectoEafit/inicio.php
@@ -8,10 +8,13 @@
     <link rel="stylesheet" href="inicio.css">
 </head>
 <body>
-    
+    <center>
+    <h2>Selecciona tu eps a continuacion y mira que lugar te queda mas favorable para atender tu emergencia:</h2>
+    <h3>Seleciona entre SURA, SAVIASALUD y NUEVA EPS.</h3>
     <div id="cuadro1">
         <form action="inicio.php" method="get">
             <input name="eps" class="input" placeholder ="EPS" type="text" onfocus="this.placeholder=''" onblur="this.placeholder='EPS'">
+            <br><br>
             <input type="submit" class="boton" value="Buscar" name="buscar">
             <?php
                 if (isset($_GET["buscar"])) {
@@ -20,11 +23,13 @@
                         header("Location:mapa_sura.html");
                     }else if ($eps=="SAVIASALUD") {
                         header("Location:mapa_saviasalud.html");
+                    }else if ($eps=="NUEVA EPS") {
+                        header("Location:mapa_nuevaeps.html");
                     }
                 }
             ?>
         </form>
     </div>
-   
+    </center>   
 </body>
 </html>
